060268fe84e9946118bb7899ca4bfd7dd5c98ced
daniel-budai <danielbudai@tutanota.com>
2024-10-22 15:55:48 +0200

    litet d

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header"; // Import Header
 import PasswordList from "../../components/PasswordList/PasswordList";
-import "./Dashboard.scss";
+import "./dashboard.scss";
 
 const DashboardPage: React.FC = () => {
   const passwords = [
